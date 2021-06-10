5d598fe772e10436d1c0475a0ca8dff2826b539b
kimtien123 <huynhthikimtien123@gmail.com>
2021-06-10 20:16:47 +0700

    Final Test

---
+++ Shopping/Shopping/Areas/Administrator/Controllers/FeedbackController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Shopping.Models;
+
+namespace Shopping.Areas.Administrator.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        QAShop1Entities3 db = new QAShop1Entities3();
+        // GET: Administrator/Feedback
+        public ActionResult Index()
+        {
+            return View(db.Feedbacks.ToList());
+        }
+    }
+}
--- Shopping/Shopping/Controllers/ContactController.cs
+++ Shopping/Shopping/Controllers/ContactController.cs
@@ -3,13 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Shopping.Models;
 
 namespace Shopping.Controllers
 {
     public class ContactController : Controller
     {
+        QAShop1Entities3 db = new QAShop1Entities3();
         // GET: Contact
         public ActionResult Contact()
+        {
+            ViewBag.SanPhamMoi = db.Products.OrderByDescending(x => x.CreatedDate).Take(4).ToList();
+            ViewBag.CoTheBanThich = db.Products.Where(m => m.CategoryID == 3).ToList();
+            return View();
+        }
+        //public int GuidId()
+        //{
+        //    var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        //    var stringChars = new char[10];
+        //    var random = new Random();
+
+        //    for (int i = 0; i < stringChars.Length; i++)
+        //    {
+        //        stringChars[i] = chars[random.Next(chars.Length)];
+        //    }
+
+        //    var finalString = new String(stringChars);
+        //    return finalString;
+        //}
+        public ActionResult Feedback(FormCollection form)
+        {
+            Feedback fb = new Feedback();
+            fb.Name = form["NameFeedback"];
+            fb.Mail = form["MailFeedback"];
+            fb.Description = form["Motafeedback"];
+            db.Feedbacks.Add(fb);
+            db.SaveChanges();
+            return RedirectToAction("Feedback_Success", "Contact");
+        }
+        public ActionResult Feedback_Success()
         {
             return View();
         }
